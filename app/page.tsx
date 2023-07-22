b95933cf712606755e68154a0c8dd1d496f0f15d
Jeff Skelton <jeff@jeffskelton.net>
2023-07-21 21:06:19 -0700

    making a small change to test deploy script

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Marquee from 'react-fast-marquee';
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
-	return (
-		<main className='flex flex-col items-center justify-between min-h-screen p-24'>
-			<Marquee className='min-h-screen text-9xl' direction='right'>
-				Coming soon...
-			</Marquee>
-		</main>
-	);
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <Marquee className="min-h-screen text-9xl" direction="right">
+        Coming very soon...
+      </Marquee>
+    </main>
+  );
 }
