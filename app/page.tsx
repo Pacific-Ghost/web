57af604e282105fd47953276b9907413187e92b2
Jeff Skelton <jeff@jeffskelton.net>
2023-07-21 21:48:08 -0700

    make a better coming soon page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Marquee from "react-fast-marquee";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <Marquee className="min-h-screen text-9xl" direction="right">
-        Coming very soon...
-      </Marquee>
+    <main className="flex flex-col items-center min-h-screen p-24">
+      <Image
+        width={500}
+        height={100}
+        src="/images/pac-ghost-logo.png"
+        alt="Pacific Ghost logo"
+      />
+      <h3 className="text-xl">Coming soon...</h3>
     </main>
   );
 }
