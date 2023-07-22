e42c385e7119a40587fca8f633cd478408522303
Jeff Skelton <jeffskelton3@users.noreply.github.com>
2023-07-21 21:10:22 -0700

    small change to test deploy (#3)
    * adds react-fast-marquee package and displays Marquee
    
    * get static build going
    
    * add deploy action
    
    * move action to workflows folder
    
    * making a small change to test deploy script
    
    ---------
    
    Co-authored-by: Matthew Eldredge <mattheweldredge@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Marquee from 'react-fast-marquee';
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
-	return (
-		<main className='flex flex-col items-center justify-between min-h-screen p-24'>
-			<Marquee className='min-h-screen text-9xl' direction='right'>
-				Coming soon...
-			</Marquee>
-		</main>
-	);
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <Marquee className="min-h-screen text-9xl" direction="right">
+        Coming very soon...
+      </Marquee>
+    </main>
+  );
 }
