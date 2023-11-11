b75a9c92325fc49582584bc3b66f1125b0077987
Jeff Skelton <jeff@jeffskelton.net>
2023-11-11 12:25:52 -0800

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, Link, Typography } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <Typography variant="h1">Pacific Ghost</Typography>
+        </Grid>
+        <Grid item xs={3} spacing={3}>
+          <Link>music</Link> <Link>bio</Link> <Link>contact</Link>{" "}
+          <Link>news</Link>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
