c01375b3b08202b78f503f81951e3010769f0ef1
Jeff Skelton <jeff@jeffskelton.net>
2023-11-11 19:45:11 -0800

    stub out pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import { CssBaseline } from "@mui/material";
 import Music from "./pages/Music.tsx";
 import { FourOhFour } from "./pages/404.tsx";
 import { HelmetProvider } from "react-helmet-async";
+import Bio from "./pages/Bio.tsx";
+import News from "./pages/News.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home />, errorElement: <FourOhFour /> },
   { path: "/music", element: <Music /> },
+  { path: "/bio", element: <Bio /> },
+  { path: "/news", element: <News /> },
 ]);
 
 function App() {
---
+++ src/pages/News.tsx
@@ -0,0 +1,5 @@
+function News() {
+    return <>TODO</>;
+}
+
+export default News;
