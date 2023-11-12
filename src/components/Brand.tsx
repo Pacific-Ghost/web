694fc14cce7b6a5233c2cd13871eb5630d969ab9
Jeff Skelton <jeff@jeffskelton.net>
2023-11-12 10:18:52 -0800

    add some responsive styling

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -4,7 +4,7 @@ function Brand({ children }: PropsWithChildren) {
   return (
     <div className="text-center font-brand uppercase text-primary">
       <div
-        className="text-background text-9xl"
+        className="text-background md:text-9xl text-7xl"
         style={{
           textShadow:
             "-1px -1px 0 #2a07bf, 1px -1px 0 #2a07bf, -1px 1px 0 #000, 1px 1px 0 #2a07bf",
@@ -12,10 +12,10 @@ function Brand({ children }: PropsWithChildren) {
       >
         Pacific
       </div>
-        <div className="bg-primary text-background font-sans mb-3">
+        <div className="bg-primary text-background font-sans lg:mb-3 mb-2">
             {children}
         </div>
-      <div className="text-9xl">Ghost</div>
+      <div className="md:text-9xl text-7xl">Ghost</div>
     </div>
   );
 }
